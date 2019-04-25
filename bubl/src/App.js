6ff7587c2f422eadc5ca218023ecbcbcc2a578db
adam <bcuz@users.noreply.github.com>
2019-04-24 21:16:59 -0400

    nonfunctioning login component, server from past proj for reference

--- bubl/src/App.js
+++ bubl/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Login from './components/Login';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Bubl App
+      <Login />
     </div>
   );
 }
---
+++ bubl/src/components/Login.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
+    
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleLogin}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>
+            Log in
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+export default Login;
