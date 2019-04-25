af6028f63ffe19d3d276b55471b4914657028764
Edgar F <eflores.ef@gmail.com>
2019-04-24 19:08:02 -0700

    Set up basic BublPage, BublMessage, InputBubl

---
+++ bubl/src/components/BublPage.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import InputBubl from './InputBubl';
+import BublMessage from './BublMessage';
+
+class BublPage extends Component {
+
+   componentDidMount() {
+      this.props.getBubls();
+   }
+
+   render() {
+      return (
+         <div className="bubl-page">
+            <h2>{this.props.bubl.bublname}</h2>
+            <ul>
+               {this.props.bubl.hashtags.map((hashtag, index) => (
+                  <div key={index}>#{hashtag}</div>
+               ))}
+            </ul>
+
+
+            {this.props.bubls.map((bubl, index) => (
+               <BublMessage key={index}/>
+            ))} 
+            <InputBubl />
+         </div>
+      )
+   }
+}
+
+export default BublPage;
