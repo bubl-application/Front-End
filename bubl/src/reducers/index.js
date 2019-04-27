d1cd5a99ed66f3352dccbf4d2ecf77b587e2ee6f
Edgar F <eflores.ef@gmail.com>
2019-04-27 09:07:59 -0700

    fixed reducer and action file mixup

--- bubl/src/reducers/index.js
+++ bubl/src/reducers/index.js
@@ -1,48 +1,78 @@
-import axios from 'axios';
+import {
+   LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE,
+   FETCH_START, FETCH_SUCCESS, FETCH_FAILURE,
+   POST_START, POST_SUCCESS, POST_FAILURE
+} from '../actions';
 
-export const LOGIN_START = "LOGIN_START";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-
-export const login = credentials => dispatch => {
-   dispatch({type: LOGIN_START});
-   axios.post('', credentials)
-      .then(res => {
-         localStorage.setItem('token', res.data.payload);
-         dispatch({type: LOGIN_SUCCESS, payload: res.data.payload});
-      })
-      .catch(err => {
-         console.log("login error:", err);
-         dispatch({type: LOGIN_FAILURE, payload: err});
-      })
+const initialState = {
+   bubls: [],
+   fetchingBubls: false,
+   postingBubls: false,
+   loggingIn: false,
+   error: ''
 }
 
-export const FETCH_START = "FETCH_START";
-export const FETCH_SUCCESS = "FETCH_SUCCESS";
-export const FETCH_FAILURE = "FETCH_FAILURE";
-
-export const getBubl = () => dispatch => {
-   dispatch({type: FETCH_START});
-   axios.get('')
-      .then(res => {
-         dispatch({type: FETCH_SUCCESS, payload: res.data})
-      })
-      .catch(err => {
-         dispatch({type: FETCH_FAILURE, payload: err})
-      })
-}
-
-export const POST_START = "POST_START";
-export const POST_SUCCESS = "POST_SUCCESS";
-export const POST_FAILURE = "POST_FAILURE";
-
-export const postFriend = bublmessage => dispatch => {
-   dispatch({type: POST_START});
-   axios.post('', bublmessage)
-      .then(res => {
-         dispatch({type: POST_SUCCESS, payload: res.data})
-      })
-      .catch(err => {
-         dispatch({type: POST_FAILURE, payload: err})
-      })
+export default (state = initialState, action) => {
+   switch (action.type) {
+      case LOGIN_START: {
+         return {
+            ...state,
+            loggingIn: true
+         }
+      }
+      case LOGIN_SUCCESS: {
+         return {
+            ...state,
+            loggingIn: false
+         }
+      }
+      case LOGIN_FAILURE: {
+         return {
+            ...state,
+            loggingIn: false
+         }
+      }
+      case FETCH_START: {
+         return {
+            ...state,
+            fetchingBubls: true
+         }
+      }
+      case FETCH_SUCCESS: {
+         return {
+            ...state,
+            fetchingBubls: false,
+            bubls: action.payload
+         }
+      }
+      case FETCH_FAILURE: {
+         return {
+            ...state,
+            fetchingBubls: false,
+            error: action.payload
+         }
+      }
+      case POST_START: {
+         return {
+            ...state,
+            postingBubls: true
+         }
+      }
+      case POST_SUCCESS: {
+         return {
+            ...state,
+            postingBubls: false,
+            bubls: action.payload
+         }
+      }
+      case POST_FAILURE: {
+         return {
+            ...state,
+            postingBubls: false,
+            error: action.payload
+         }
+      }
+      default: 
+         return state;
+   }
 }
