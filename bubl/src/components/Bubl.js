e2fd67b0f10a2cf3767be4aaffec71b99092c111
Edgar F <eflores.ef@gmail.com>
2019-05-01 18:54:34 -0700

    resolved conflict

--- bubl/src/components/Bubl.js
+++ bubl/src/components/Bubl.js
@@ -20,10 +20,6 @@ class Bubl extends Component {
       return (
          <div>
             <h2>Bubl</h2>
-<<<<<<< HEAD
-            <h3>{this.props.match.params.id}</h3>
-            {this.props.bubls[this.props.match.params.id - 1].messages.map(message => (
-=======
             {/* {this.props.bubls.map(bubl => (
                <>
                   {bubl.messages.map(message => (
@@ -35,7 +31,6 @@ class Bubl extends Component {
             ))} */}
 
             {this.props.bubls.length > 0 && this.props.bubls[this.props.match.params.id - 1].messages.map(message => (
->>>>>>> master
                <div>
                   {message.username}: {message.text}
                </div>
