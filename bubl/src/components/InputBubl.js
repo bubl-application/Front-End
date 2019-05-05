ccc488e20a7a9c7199f0a5cdd4ec2137414ed097
adam <bcuz@users.noreply.github.com>
2019-05-05 15:48:26 -0400

    handling thread_id differently. better? idk

--- bubl/src/components/InputBubl.js
+++ bubl/src/components/InputBubl.js
@@ -4,11 +4,13 @@ import { addComment, getComments } from '../actions';
 import {Button, Input} from '../StyledComponents';
 
 class InputBubl extends Component {
+   // why does it need a school_id and bubl_id
+   // to post a comment?
    state = {
       comments: '',
       school_id: '',
       student_id: '',
-      thread_id: '',
+      thread_id: parseInt(this.props.id),
       bubl_id: '',
    }
 
@@ -16,7 +18,7 @@ class InputBubl extends Component {
     this.setState({
       school_id: nextProps.school_id,
       student_id: nextProps.student_id,
-      thread_id: nextProps.thread_id,
+      // thread_id: nextProps.thread_id,
       bubl_id: nextProps.bubl_id
     });
 }
@@ -31,8 +33,8 @@ class InputBubl extends Component {
    addNewBublMessage = e => {
       e.preventDefault();
       if (this.state.comments !== '') {
-         this.props.addComment(this.state, this.props.id);
-         this.props.getComments(this.props.id)
+         this.props.addComment(this.state);
+         this.props.getComments(this.state.thread_id)
       }
       this.setState({
          comments: ""
@@ -61,7 +63,7 @@ const mapStateToProps = state => {
       school_id: state.school_id,
       student_id: state.student_id,
       bubl_id: state.bubl_id,
-      thread_id: state.thread_id,
+      // thread_id: state.thread_id,
 
    }
 }
